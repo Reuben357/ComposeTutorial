2f89065c2674954e84113b0c59fff19d8a966929
Reuben <gathiireuben@gmail.com>
2024-01-04 08:44:28 +0300

    Calling the MessageCard function in the setContent block that defines the activity's layout where composable functions are called.

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -22,7 +22,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MessageCard(
+                        Message(
+                            "Android", "Jetpack Compose"
+                    ))
                 }
             }
         }
